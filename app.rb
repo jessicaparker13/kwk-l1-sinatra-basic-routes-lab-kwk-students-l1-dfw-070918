cdcc31a0019fefe7300563ad0e047d8ff78c5c48
jessicaparker13 <parkerjessica2001@gmail.com>
2018-07-19 18:16:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World"
+  end
+end 
+
   get '/name' do
     "My name is Jess."
   end 
   
   get '/hometown' do
-    "My hometown is LA."
+    "My hometown is Fort Worth."
   end 
   
   get '/favorite-song' do 
-    "My favorite song is your face."
+    "My favorite song is ummmm."
   end 
 end
